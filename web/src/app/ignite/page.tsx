4903d981195e31dd7bfc39d1d40124587fed72b1
RafaelEtiene <rafaellimaetiene@gmail.com>
2023-05-15 23:27:03 -0300

    Configurando os ambientes do backend e frontend

---
+++ web/src/app/ignite/page.tsx
@@ -0,0 +1,3 @@
+export default function Ignite() {
+  return <h1 className="text-2xl">Ignite</h1>
+}
---
+++ web/src/app/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@/components/Button'
+
+export default function Home() {
+  return <div className="h-screen bg-zinc-900">oi</div>
+}
