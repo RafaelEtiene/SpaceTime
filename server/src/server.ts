1f06026767769f4666164ff594418ec469b7e16e
RafaelEtiene <rafaellimaetiene@gmail.com>
2023-05-17 21:52:10 -0300

    Criando a rota de autenticação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
 /* eslint-disable prettier/prettier */
+import 'dotenv'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
 
 
 const app = fastify();
@@ -9,6 +11,7 @@ app.register(cors, {
   origin: true, // todas as URLs de front end poderão acessar nosso back-end
 })
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
