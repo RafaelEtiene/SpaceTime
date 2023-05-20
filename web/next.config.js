a4d0f088c4a1b5e558673df3593cbff87938b0ed
RafaelEtiene <rafaellimaetiene@gmail.com>
2023-05-20 15:29:43 -0300

    Finalizando a listagem das memórias de cada usuário

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3333',
+        pathname: '/uploads/**',
+      },
+    ],
   },
 }
 
--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -2,7 +2,7 @@ import { NewMemoryForm } from '@/components/NewMemoryForm'
 import { ChevronLeft } from 'lucide-react'
 export default function NewMemory() {
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-1 flex-col gap-4 p-16">
       <a
         href="/"
         className="flex items-center gap-1 text-sm text-gray-200  hover:text-gray-100"
